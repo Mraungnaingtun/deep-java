df8aabf9797ab08d5218051a8515d017f1fd0eca
Purwatsate <ant.pwt@gamil.com>
2024-11-18 09:10:21 +0630

    update

--- src/main/java/org/logant/Main.java
+++ src/main/java/org/logant/Main.java
@@ -1,35 +1,8 @@
 package org.logant;
 
-import org.logant.OOP.Inheritance;
-
 public class Main {
 
     public static void main(String[] args) {
-    //    new Block();
-    //    new Block();
 
-
-        Inheritance inheritance = new Inheritance();
-        inheritance.showWhatIHave();
     }
-}
-
-class Block {
-
-    {
-        System.out.println("No Static Block1");
-    }
-
-    {
-        System.out.println("No Static Block2");
-    }
-
-    static {
-        System.out.println("Static Block1");
-    }
-
-    static {
-        System.out.println("Static Block2");
-    }
-
 }
