88ecf2daa6b25dca9cd17b2cef1a7c55dc1bb4eb
Purwatsate <ant.pwt@gamil.com>
2024-11-18 09:26:18 +0630

    update

---
+++ src/main/java/org/logant/Servlet/HelloServlet.java
@@ -0,0 +1,18 @@
+package org.logant.Servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/api/hello")
+public class HelloServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.getWriter().write("{\"message\": \"Hello, World!\"}");
+    }
+}
